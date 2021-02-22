01da2f4fc1f30416efcc84dff2e49783d6c0ee4d
Davinderpal <davinder.batth77@gmail.com>
2021-02-22 17:32:13 -0500

    Project Outline Complete

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { ShowInfoComponent } from './show-info/show-info.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchResultsComponent,
+    ShowInfoComponent,
+    ShowDetailsComponent,
+    WatchlistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
