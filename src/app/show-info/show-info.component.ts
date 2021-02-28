f9c71e73d12baab548d0f7837e0dcb977898dfbb
Davinderpal singh <Davinder.batth77@gmail.com>
2021-02-28 12:47:31 -0500

    Added Show Info Cotent

--- src/app/show-info/show-info.component.ts
+++ src/app/show-info/show-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Show } from '../models/show.model';
 
 @Component({
   selector: 'app-show-info',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInfoComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+  show: Show;
+
+  constructor() {
+    this.show = new Show({
+      name: 'Doctor Who',
+      language: 'English',
+      genres: ['Sci-Fi', 'Action', 'Adventure'],
+      id: 210,
+      summary: 'A show about a time travelling alien called The Doctor.',
+      status: 'ongoing',
+      image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
+    });
+  }
 
   ngOnInit(): void {
   }
-
 }
