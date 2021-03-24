0ad48e44e0b647cd4a2ed79b0032c128c71efdf2
Davinderpal singh <Davinder.batth77@gmail.com>
2021-03-23 22:24:19 -0400

    Added routes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  query = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  search(): void {
+    this.router.navigate(['search', this.query]);
   }
-
 }
