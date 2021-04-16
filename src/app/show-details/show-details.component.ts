8446c8b2e8fde8e5f69e1c49b5852ed327b14429
davinder1642 <davinder.batth77@gmail.com>
2021-04-15 20:05:34 -0400

    Updated table styles and content

--- src/app/show-details/show-details.component.ts
+++ src/app/show-details/show-details.component.ts
@@ -3,25 +3,30 @@ import { Episode } from '../models/episode.model';
 import { Show } from '../models/show.model';
 import {ActivatedRoute} from '@angular/router';
 import {DummyService} from '../services/dummy.service';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-show-details',
-  templateUrl: './show-details.component.html',
-  styleUrls: ['./show-details.component.css']
+  @@ -11,17 +12,22 @@ import {DummyService} from '../services/dummy.service';
 })
 export class ShowDetailsComponent implements OnInit {
   show: Show;
-  episodes: Array<Episode>;
+  episodes: MatTableDataSource<Episode>;
   tableHeaders: Array<string>;
   constructor(private route: ActivatedRoute, private dummyService: DummyService) {
-    this.tableHeaders = ['season', 'episode', 'name', 'aired', 'summary'];
+    this.tableHeaders = ['number', 'name', 'aired', 'summary'];
   }
 
   ngOnInit(): void {
     this.route.params.subscribe((p) => {
       this.dummyService.getShow(p.id).subscribe((show) => { this.show = show; });
-      this.dummyService.getEpisodes().subscribe((eps) => { this.episodes = eps; });
+      this.dummyService.getEpisodes().subscribe((eps) => { this.episodes = new MatTableDataSource<Episode>(eps); });
     });
+  }
+
+  applyFilter(e: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.episodes.filter = filterValue.trim().toLowerCase();
   }
 
 }
